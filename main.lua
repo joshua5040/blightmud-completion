680197c69d7466134fbff951bb52cdd01ea4e553
Joshua James <94586141+joshua5040@users.noreply.github.com>
2021-11-21 16:23:02 -0800

    Added add_plugin command file completion

--- main.lua
+++ main.lua
@@ -10,15 +10,15 @@ trigger.add("^(.+)$", {}, function(_, line)
     output_history[output_history.last] = line:line()
 end)
 
-local function complete_load_command(input)
-  local partial_path = string.sub(input, 6)
+local function complete_filepath(partial_path, prefix)
+  local prefix = prefix or ''
   local completions = {}
   local response = core.exec("compgen -f " .. partial_path)
   for line in response:stdout():gmatch("([^\n]+)") do
     if core.exec('file ' .. line):stdout():match('directory') then
-      table.insert(completions, "/load " .. line .. '/')
+      table.insert(completions, prefix .. line .. '/')
     else
-      table.insert(completions, "/load " .. line .. ' ')
+      table.insert(completions, prefix .. line .. ' ')
     end
   end
   return completions
@@ -34,21 +34,25 @@ local function complete_on_mud_output(input)
     local complete_word_matches = complete_word:match(output_history[i])
     if complete_word_matches then
       for j = #complete_word_matches, 1, -1 do
-        table.insert(completions, input_start .. complete_word_matches[j]) end
+        table.insert(completions, input_start .. complete_word_matches[j])
+      end
     end
   end
   return completions
 end
 
 local function complete(input)
-  local load_re = regex.new("^/load")
+  local file_command_re = regex.new('(^/load |^/add_plugin )([^\n]+|)$')
+  local file_command_matches = file_command_re:match(input)
   local completions = {}
-  if load_re:test(input) then
-    completions = complete_load_command(input)
+  if file_command_re:test(input)  then
+    local filepath = file_command_matches[3]
+    local command = file_command_matches[2]
+    completions = complete_filepath(filepath, command)
   else
     completions = complete_on_mud_output(input)
   end
-  return completions
+  return completions, true
 end
 
-blight.on_complete(complete, true)
+blight.on_complete(complete)
